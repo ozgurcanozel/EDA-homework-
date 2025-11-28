4b6dce58531617b9d8b70cf5230ece5dbafa836e
Özgürcan Özel <ozgurozel365@gmail.com>
2025-11-28 19:17:33 +0300

    checked if the data set is working

--- wine_analysis.py
+++ wine_analysis.py
@@ -4,3 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("WineQT.csv")
+
+print(data.head())
+print(data.describe())
+print(data.info())
+print(data.isnull().sum())
