cee0793141d0845eaf380f269aefd2eb3516be68
Özgürcan Özel <ozgurozel365@gmail.com>
2025-11-28 19:24:47 +0300

    implemented quality score value counts

--- wine_analysis.py
+++ wine_analysis.py
@@ -14,3 +14,7 @@ print(data.isnull().sum())
 plt.figure(figsize = (8,8))
 sns.heatmap(data.corr(numeric_only=True), annot = True) # Which feature affects quality the most? = alcohol
 plt.show()
+
+# How do you list how many different score types there are in the quality column (e.g. 3, 4, 5, 6...) and how many wines there are with each score?
+print(data.columns)
+print(data['quality'].unique())
