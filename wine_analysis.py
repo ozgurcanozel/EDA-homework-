5c05a1d0891c51ab464759fafc5e8f37a4cdb38a
Özgürcan Özel <ozgurozel365@gmail.com>
2025-11-28 20:33:22 +0300

    kdeplot

--- wine_analysis.py
+++ wine_analysis.py
@@ -10,30 +10,51 @@ print(data.describe())
 print(data.info())
 print(data.isnull().sum())
 
-# correlation
-plt.figure(figsize = (8,8))
-sns.heatmap(data.corr(numeric_only=True), annot = True) # Which feature affects quality the most? = alcohol
-plt.show()
+# # correlation
+# plt.figure(figsize = (8,8))
+# sns.heatmap(data.corr(numeric_only=True), annot = True) # Which feature affects quality the most? = alcohol
+# plt.show()
+#
+# # How do you list how many different score types there are in the quality column (e.g. 3, 4, 5, 6...) and how many wines there are with each score?
+# print(data.columns)
+# print(data['quality'].unique())
+#
+# # Can you draw a histogram to see the distribution of quality scores for the wines? (Is the data balanced or clustered at specific scores?)
+# sns.countplot(x='quality', data=data)
+# plt.title("Count of quality")
+# plt.xlabel("Quality")
+# plt.ylabel("Count")
+# plt.show()
+#
+# # Can you draw the distribution of alcohol percentages and add the density curve to it?
+#
+# sns.displot(x="alcohol", data=data, kind = "kde")
+# plt.xlabel("Alcohol")
+# plt.show()
+#
+# data["quality"].value_counts().plot(kind="pie")
+# plt.show()
+#
+# sns.boxplot( x="free sulfur dioxide", y="quality", data=data)
+# plt.show()
 
-# How do you list how many different score types there are in the quality column (e.g. 3, 4, 5, 6...) and how many wines there are with each score?
-print(data.columns)
-print(data['quality'].unique())
+column = data.columns
+(fig, ax) = plt.subplots(4,4, figsize = (25,25))
+ax = ax.flatten()
 
-# Can you draw a histogram to see the distribution of quality scores for the wines? (Is the data balanced or clustered at specific scores?)
-sns.countplot(x='quality', data=data)
-plt.title("Count of quality")
-plt.xlabel("Quality")
-plt.ylabel("Count")
-plt.show()
+for i,column in enumerate(column):
+    sns.kdeplot(
+        data= data,
+        x = column,
+        hue= data.quality,
+        ax= ax[i]
 
-# Can you draw the distribution of alcohol percentages and add the density curve to it?
+    )
+    ax[i].set_title(f"{column} Distrubution")
+    ax[i].set_xlabel(None)
 
-sns.displot(x="alcohol", data=data, kind = "kde")
-plt.xlabel("Alcohol")
-plt.show()
 
-data["quality"].value_counts().plot(kind="pie")
-plt.show()
 
-sns.boxplot( x="free sulfur dioxide", y="quality", data=data)
+for i in range(i+1, len(ax)):
+    ax[i].axis("off")
 plt.show()
